f02709be4ec453d24959ddfb082e15f67b76a51b
Eric Brelsford <ebrelsford@gmail.com>
2015-04-15 16:15:13 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var map = L.map('map').setView([40.70, -73.96], 11);
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/ebrelsford.ho06j5h0/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>'
+    }).addTo(map);
+});
